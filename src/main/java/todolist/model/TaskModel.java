40a7cdb378590ae6f65c6e074b213d6fc170b23a
Amvrosov <amvrosovalex@gmail.com>
2017-06-26 16:02:06 +0800

    updating Spring and Hibernate versions and fixing related bugs

--- src/main/java/todolist/model/TaskModel.java
+++ src/main/java/todolist/model/TaskModel.java
@@ -11,7 +11,7 @@ import java.util.Date;
 public class TaskModel implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column
